a1abe92fce2ab78f72cca63cb55fce000d6fbd11
benisamuel <benisamuel566@gmail.com>
2025-08-15 23:37:20 +0200

    Structuring home tab

--- src/screens/tabs/home/Home.tsx
+++ src/screens/tabs/home/Home.tsx
@@ -1,6 +1,16 @@
-import { Dimensions, Platform, SafeAreaView, StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import { StatusBar } from 'expo-status-bar'
+import {
+  Dimensions,
+  Platform,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+import Header from "../../../components/home/header/Header";
+import Inform from "../../../components/home/inform/Inform";
+import AvailableBuses from "../../../components/home/buses/AvailableBuses";
 
 const { height, width } = Dimensions.get("window");
 
@@ -8,18 +18,26 @@ const Home = () => {
   return (
     <SafeAreaView style={styles.container}>
       <StatusBar />
+      <Header />
 
-      <Text>Name</Text>
+      <View style={styles.container_2}>
+        <Inform />
+        <AvailableBuses />
+      </View>
     </SafeAreaView>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: "red",
-    paddingTop: Platform.OS === "android" ? height * 0.035 : null,
-    paddingHorizontal: width * 0.035
-  }
-})
+    backgroundColor: "#00CC99",
+    paddingTop: Platform.OS === "android" ? height * 0.05 : null,
+    flex: 1,
+  },
+  container_2: {
+    backgroundColor: "#F9FAFF",
+    flex: 1
+  },
+});
