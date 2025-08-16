c3708188371179970605e2f8c65bbf7238025e96
benisamuel <benisamuel566@gmail.com>
2025-08-16 19:31:21 +0200

    added improved code layout

--- src/types/user/user.type.ts
+++ src/types/user/user.type.ts
@@ -1,12 +1,12 @@
 import { ImageSourcePropType } from "react-native";
 
 type User = {
-    id: number;
-    userprofile: ImageSourcePropType;
-    username: string;
-    email: string;
-    phone: number;
-    password: number;
-}
+  id: number;
+  userprofile: ImageSourcePropType;
+  username: string;
+  email: string;
+  phone: number;
+  password: number;
+};
 
 export default User;
