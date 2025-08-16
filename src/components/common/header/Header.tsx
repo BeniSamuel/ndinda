ba508a5ed4d746ceafda7bf15cc7f1596b6a3d44
benisamuel <benisamuel566@gmail.com>
2025-08-16 19:12:36 +0200

    modified header component for better experience

--- src/components/common/header/Header.tsx
+++ src/components/common/header/Header.tsx
@@ -1,22 +1,38 @@
-import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { StyleSheet, Text, TouchableOpacity, View, Image, ImageSourcePropType, Dimensions } from "react-native";
 import React from "react";
 
+const { height, width } = Dimensions.get("window");
+
 type HeaderProp = {
-  screen_name: string;
+  screen_name?: string;
+  image?: ImageSourcePropType
 };
 
-const Header: React.FC<HeaderProp> = ({ screen_name }) => {
+const Header: React.FC<HeaderProp> = ({ screen_name, image }) => {
   return (
-    <View>
+    <View style={styles.container}>
       <View>
-        <TouchableOpacity></TouchableOpacity>
+        <TouchableOpacity>
+          <Image source={require("../../../../assets/common/left-arrow-icon.png")}/>
+        </TouchableOpacity>
         <Text>{screen_name}</Text>
       </View>
-      <View></View>
+      <View>
+        <Image source={image} />
+      </View>
     </View>
   );
 };
 
 export default Header;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#00CC99",
+    paddingVertical: height * 0.02,
+    paddingHorizontal: width * 0.035,
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "space-between"
+  }
+});
