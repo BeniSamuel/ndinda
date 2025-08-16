076ff5abe0181ef6b734783b9a7fd22fbb5c66ab
benisamuel <benisamuel566@gmail.com>
2025-08-16 19:19:52 +0200

    added tablayout

--- src/layout/TabLayout.tsx
+++ src/layout/TabLayout.tsx
@@ -1,16 +1,42 @@
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import { StatusBar } from 'expo-status-bar'
+import {
+  Dimensions,
+  ImageSourcePropType,
+  Platform,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
+import React, { ReactNode } from "react";
+import { StatusBar } from "expo-status-bar";
+import Header from "../components/common/header/Header";
 
-const TabLayout = () => {
+const { width, height } = Dimensions.get("window");
+
+type TabLayoutProp = {
+  children: ReactNode;
+  screen_name?: string;
+  image?: ImageSourcePropType;
+};
+
+const TabLayout: React.FC<TabLayoutProp> = ({
+  children,
+  screen_name,
+  image,
+}) => {
   return (
-    <SafeAreaView>
-        <StatusBar />
-        
+    <SafeAreaView style={styles.container}>
+      <StatusBar />
+      <Header screen_name={screen_name} image={image} />
+      <View>{children}</View>
     </SafeAreaView>
-  )
-}
+  );
+};
 
-export default TabLayout
+export default TabLayout;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: Platform.OS === "android" ? height * 0.035 : null,
+  },
+});
