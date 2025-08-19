cab2f5827905d205a0ddc1ddd1aea6641cc0969f
benisamuel <benisamuel566@gmail.com>
2025-08-19 10:39:19 +0200

    added notification all notificatoin component on notification screen

--- src/screens/tabs/notification/Notification.tsx
+++ src/screens/tabs/notification/Notification.tsx
@@ -1,17 +1,21 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import TabLayout from '../../../layout/TabLayout'
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import TabLayout from "../../../layout/TabLayout";
+import notifications from "../../../data/notification/notification.data";
+import Empty from "../../../components/notification/empty-notification/Empty";
+import Full from "../../../components/notification/full-notification/Full";
 
 const Notification = () => {
   return (
-    <TabLayout screen_name='Notification'>
-      <View>
-        <Text>Not</Text>
-      </View>
+    <TabLayout
+      screen_name="Notification"
+      image={require("../../../../assets/onboarding/user-profile-image.png")}
+    >
+      {notifications.length === 0 ? <Empty /> : <Full />}
     </TabLayout>
-  )
-}
+  );
+};
 
-export default Notification
+export default Notification;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
