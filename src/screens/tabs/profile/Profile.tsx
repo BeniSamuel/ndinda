86f74cd71833733c90c983e31dc5e21116142a56
benisamuel <benisamuel566@gmail.com>
2025-08-26 12:57:49 +0200

    added profile content

--- src/screens/tabs/profile/Profile.tsx
+++ src/screens/tabs/profile/Profile.tsx
@@ -1,12 +1,21 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View } from "react-native";
 import React from "react";
 import TabLayout from "../../../layout/TabLayout";
+import UserImage from "../../../components/profile/UserImage";
+import UserInform from "../../../components/profile/UserInform";
+import UserPass from "../../../components/profile/UserPass";
+import DeleteAccount from "../../../components/profile/DeleteAccount";
+import Logout from "../../../components/profile/Logout";
 
 const Profile = () => {
   return (
     <TabLayout screen_name="User Profile">
-      <View>
-        <Text>Profile</Text>
+      <View style={styles.container}>
+        <UserImage />
+        <UserInform />
+        <UserPass />
+        <DeleteAccount />
+        <Logout />
       </View>
     </TabLayout>
   );
@@ -14,4 +23,10 @@ const Profile = () => {
 
 export default Profile;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    display: "flex",
+    flexDirection: "column",
+    gap: 15,
+  },
+});
