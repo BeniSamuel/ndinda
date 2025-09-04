87cdb9dae06e05c963987f3bf162994b959cfd79
benisamuel <benisamuel566@gmail.com>
2025-09-04 19:43:20 +0200

    added status type on stop

--- src/types/stop/stop.type.ts
+++ src/types/stop/stop.type.ts
@@ -1,8 +1,11 @@
+import { Status } from "../../enums/status.enum";
+
 type Stop = {
   id: number;
   place: string;
   details: string;
   bus_id: number;
+  status: Status;
 };
 
 export default Stop;
