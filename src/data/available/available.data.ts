5833c7af175fa38018142dc7795ec069d4dc2776
benisamuel <benisamuel566@gmail.com>
2025-09-04 16:34:22 +0200

    added additional package to improve application

--- src/data/available/available.data.ts
+++ src/data/available/available.data.ts
@@ -1,5 +1,41 @@
 import Bus from "../../types/bus/bus.type";
 
-const available_buses: Bus[] = [];
+const available_buses: Bus[] = [
+  {
+    id: 1,
+    company_name: "Yahoo Car",
+    plate_number: "RAB904X",
+    journey_number: 105,
+    journey_details: "Kimisange-Gikondo-DownTown",
+  },
+  {
+    id: 2,
+    company_name: "Yahoo Car",
+    plate_number: "RAB904X",
+    journey_number: 105,
+    journey_details: "Kimisange-Gikondo-DownTown",
+  },
+  {
+    id: 3,
+    company_name: "Yahoo Car",
+    plate_number: "RAB904X",
+    journey_number: 105,
+    journey_details: "Kimisange-Gikondo-DownTown",
+  },
+  {
+    id: 4,
+    company_name: "Yahoo Car",
+    plate_number: "RAB904X",
+    journey_number: 105,
+    journey_details: "Kimisange-Gikondo-DownTown",
+  },
+  {
+    id: 5,
+    company_name: "Yahoo Car",
+    plate_number: "RAB904X",
+    journey_number: 105,
+    journey_details: "Kimisange-Gikondo-DownTown",
+  },
+];
 
 export default available_buses;
