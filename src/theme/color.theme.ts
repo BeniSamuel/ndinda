55d072a3ecb8492d122e462794f8452c9082c59b
benisamuel <benisamuel566@gmail.com>
2025-09-04 19:42:50 +0200

    added status colors

--- src/theme/color.theme.ts
+++ src/theme/color.theme.ts
@@ -1,7 +1,9 @@
 export const lightTheme = {
   background: "#F9FAFF",
   title_text: "#000",
-  green_color: "#00CC99"
+  green_color: "#00CC99",
+  red_status_color: "#A52A2A",
+  green_status_color: "#00D247"
 };
 
 export const darkTheme = {
